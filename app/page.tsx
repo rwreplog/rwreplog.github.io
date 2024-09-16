beb59b1828b0f28047d25e2ff0d603e338c6aedd
Ryan Replogle <ryan.replogle@gmail.com>
2024-09-15 21:34:07 -0400

    feat: update navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
 import Grid from "@/components/grid";
 import Hero from "@/components/hero";
 import { FloatingNav } from "@/components/ui/floatingNav";
+import { navItems } from "@/data";
 import { FaHouse } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[
-            { name: "Home", link: "/", icon: <FaHouse /> },
-            { name: "About", link: "#about" },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
+
         <Hero></Hero>
         <Grid></Grid>
       </div>
