1da46a126674b9f0b15a18d725d4ff9caea209c5
Ryan Replogle <ryan.replogle@gmail.com>
2024-09-15 20:58:02 -0400

    feat: update about

--- components/ui/magicButton.tsx
+++ components/ui/magicButton.tsx
@@ -4,6 +4,7 @@ const MagicButton = ({
   title,
   icon,
   position,
+  handleClick,
   otherClasses
 }: {
   title: string;
@@ -14,11 +15,20 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className={`inline-flex h-12 md:w-60 md:mt-10 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(95deg,#000103,15%,#1e2631,85%,#000103)] bg-[length:200%_100%] px-7 font-medium text-slate-400 transition-colors gap-2 ${otherClasses}`}
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
+      onClick={handleClick}
     >
-      {position === "left" && icon}
-      {title}
-      {position === "right" && icon}
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+
+      {/* remove px-3 py-1, add px-5 gap-2 */}
+      <span
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+      >
+        {position === "left" && icon}
+        {title}
+        {position === "right" && icon}
+      </span>
     </button>
   );
 };
