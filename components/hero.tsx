f32a8dbee6e58a111ee9099a7adf973b8c273311
Ryan Replogle <ryan.replogle@gmail.com>
2024-09-15 22:17:57 -0400

    Feat: Logo work (#42)
    * logo
    
    * feat: logo work

--- components/hero.tsx
+++ components/hero.tsx
@@ -21,6 +21,9 @@ const Hero = () => {
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.01] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
+      <div className="h-10 w-3/6 flex items-center justify-center relative ml-auto mr-auto -mb-10 mt-10">
+        <img src="/rwr_logo.svg" />
+      </div>
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
